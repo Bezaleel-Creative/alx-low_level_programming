9a9b34c2af922ca490d785dcea63340a11c6e0fd
Kazeem Bolaji <kazeem.s.bolaji@gmail.com>
2023-06-20 12:41:51 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * main - print _putchar
+ * main - print_putchar
  * is written by mbah
  * Return: 0
  */
 
-
-int main(void){
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,4 +17,5 @@ int main(void){
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
+	
